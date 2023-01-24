e0f492eba18eb07ad0011f668f59a53796bbc299
Ulises <larrabeulises@gmail.com>
2023-01-24 17:59:52 -0300

    Cambios en el responsive y en Firebase.js

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -2,6 +2,8 @@ import "../../css/CartContainer.css"
 import { useContext, useEffect, useState } from "react"
 import { cartContext } from "../../storage/cartContext"
 import { NoProductsAlert } from "../CartWidget/NoProducts/NoProductsAlert";
+import { createOrder } from "../../services/firebase";
+import Swal from 'sweetalert2';
 
 const CartContainer = () => {
     const [newCart, setNewCart] = useState([]);
@@ -11,19 +13,46 @@ const CartContainer = () => {
     useEffect(() => {
         setNewCart(cart)
         setLoader(...cart)
-    }, [cart])
+    }, [cart]);
+
+    const handleCheckout = () => {
+        const items = cart.map (({id, price, title, quantity}) => ({id, price, title, quantity}));
+
+        const order = {
+            buyer:{
+                name:"Ulises",
+                email:"ejemplomail@gmail.com",
+                phone: 1265327653,
+            },
+            items: items,
+            total: totalPrice(),
+            date: new Date(),
+        }
+
+        setTimeout(() => {
+            clearCart()
+        }, 2000);
+
+        createOrder(order).then((id) => Swal.fire({
+            icon: 'success',
+            title: 'Su compra ha sido realizada',
+            text: `Su id de compra es: ${id}`,
+        }));
+    }
+
     
     if (loader === undefined){
         return <NoProductsAlert/>
     } else{
         return ( 
         <>
-            <div className="table-cart">
+
+<div className="table-cart">
                 <div className="table-cart-inf">
                     <div className="overflow-x-auto">
-                        <table className="table w-full table-compact" id=" table-color">
+                        <table className="table-orig">
                             <thead>
-                                <tr className="table-color">
+                                <tr>
                                     <th></th>
                                     <th> Nombre</th>
                                     <th>Precio</th>
@@ -35,32 +64,27 @@ const CartContainer = () => {
                             <tbody>
                                 {newCart.map((c) => {
                                     return(
-                                        <tr className="hover table-color" key={c.id}>
-                                            <td ><img src={c.img} alt={c.img} className="img-cart" /></td>
-                                            <td >{c.title}</td>
-                                            <td>${c.price}</td>
-                                            <td >{c.quantity}</td>
-                                            <td >${c.quantity * c.price}</td>
+                                        <tr key={c.id}>
+                                            <td><img src={c.img} alt={c.img} className="img-cart" /></td>
+                                            <td> <strong>{c.title}</strong></td>
+                                            <td> <strong>${c.price}</strong></td>
+                                            <td> <strong>{c.quantity}</strong></td>
+                                            <td> <strong>${c.quantity * c.price}</strong></td>
                                             <td>
-                                                <button onClick={() => removeItem(c)} className="btn btn-table-cart bg-orange-600 button-table"> X </button>
+                                                <button onClick={() => removeItem(c)} className="btn button-table"> X </button>
                                             </td>
                                         </tr>
-                )})}
-                                        <tr>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td className="total-price">Precio total: ${totalPrice()}  </td>
-                                            
-                                        </tr>
+                                    )})}
                             </tbody>
                         </table>
 
+                        <div className="total-purchase">
+                            <p>El total de tu compra es de <strong>${totalPrice()}</strong></p> 
+                        </div>
+
                         <div className="buttons-cart">
-                            <button className="btn bg-orange-600 button-table" onClick={() => clearCart()}>borrar carrito</button>
-                            <button className="btn bg-orange-600 button-table end-purchase">Finalizar Compra</button>
+                            <button className="btn button-table clear-cart" onClick={() => clearCart()}>borrar carrito</button>
+                            <button className="btn button-table end-purchase" onClick={handleCheckout} >Finalizar Compra</button>
                         </div>
         
                         
